462bea84062d45ed7a5c99d17164dbf86ec43bf4
maxwellpalmeida <46318799+maxwellpalmeida@users.noreply.github.com>
2019-03-16 09:07:49 -0300

    Crawler adjustments. Project rename. Trash code comments cleaning

---
+++ UnBWebCrawler/Classes/Models/Departments.cs
@@ -0,0 +1,72 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UnBWebCrawler.Classes.Models
+{
+    public class Departments : Common.PageContent
+    {
+        public Departments(string url) : base(url)
+        { }
+
+        public Task CompletedTask { get; set; }
+
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private List<Disiplines> _Disciplines = new List<Disiplines>();
+        public List<Disiplines> Disciplines
+        {
+            get { return _Disciplines; }
+            set { _Disciplines = value; }
+        }
+
+        public async Task LoadDisciplinesAsync()
+        {
+            var tableDisciplines = (base.Content).DocumentNode.Descendants("table").Where(w => w.GetAttributeValue("id", "").Equals("datatable")).FirstOrDefault();
+            if (tableDisciplines != null)
+            {
+                var trs = tableDisciplines.Descendants("tr");
+                if (trs != null)
+                {
+                    foreach (var tr in trs)
+                    {
+                        if (tr.Descendants("th").Any()) // Ignore header
+                            continue;
+
+                        var a = tr.Descendants("td").FirstOrDefault(w => w.Descendants("a")?.Any() == true)?.Descendants("a")?.FirstOrDefault();
+                        if (a != null)
+                        {
+                            string Codigo;
+                            string conteudo = a.GetAttributeValue("href", "");
+
+                            var rgx = new Regex(@"cod=\d+", RegexOptions.IgnoreCase);
+                            var sub = rgx.Match(conteudo);
+                            Codigo = sub.Groups[0].ToString().Replace("cod=", "");
+
+                            this.Disciplines.Add(new Disiplines(url: "https://wwwsec.serverweb.unb.br/graduacao/" + conteudo)
+                            {
+                                Name = a.InnerText,
+                                Code = Codigo,
+                                Vacant = false
+                            });
+                        }
+                    }
+                }
+            }
+
+            foreach (var disciplina in this.Disciplines)
+            {
+                await disciplina.LoadAsync();
+            }
+        }
+    }
+}
